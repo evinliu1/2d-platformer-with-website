1cb565472f0b5f7a65ba18d695f21185bcd687a9
nathansteves <65842149+nathansteves@users.noreply.github.com>
2022-07-28 16:04:12 -0400

    Add files via upload

---
+++ project_db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`Admin` (
+  `AdminID` VARCHAR(45) NOT NULL,
+  `AdminName` VARCHAR(45) NOT NULL,
+  `AdminPass` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`AdminID`));
+
+INSERT INTO Admin (AdminID, AdminName, AdminPass)
+VALUES('nsteves', 'Nathan Steves', 'Admin123'),
+	('elieu', 'Evan Liu', 'Admin123'),
+	('nlovett', 'Nurielle Lovett', 'Admin123'),
+	('lnguyen', 'Lan Nguyen', 'Admin123'),
+	('hphan', 'Henry Phan', 'Admin123');
+
+CREATE TABLE IF NOT EXISTS `mydb`.`Users` (
+  `UserID` INT NOT NULL,
+  `UserName` VARCHAR(45) NOT NULL,
+  `UserPass` VARCHAR(45) NOT NULL,
+  `DownloadedGame` BINARY(1) NULL,
+  PRIMARY KEY (`UserID`));
