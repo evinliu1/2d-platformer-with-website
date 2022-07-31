77fb469cd0451b6a5f2fe6207a9599020ead8cc2
nathansteves <65842149+nathansteves@users.noreply.github.com>
2022-07-31 17:48:07 -0400

    Update login.js

--- login.js
+++ login.js
@@ -6,9 +6,27 @@ const path = require('path');
 const connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '',
-    database : 'nodelogin'
+    password : 'root',
+    database : 'glitchdb'
 });
+
+// Connecting to database
+connection.connect(function(err) {
+    if(err){
+        console.log("Error in the connection")
+        console.log(err)
+    }
+    else{
+        console.log(`Database Connected`)
+        connection.query(`SHOW DATABASES`,
+            function (err, result) {
+                if(err)
+                    console.log(`Error executing the query - ${err}`)
+                else
+                    console.log("Result: ",result)
+            })
+    }
+})
 
 const app = express();
 
@@ -65,4 +83,4 @@ app.post('/auth', function(request, response) {
     }
 });
 
-app.listen(3000);
+app.listen(3306);
