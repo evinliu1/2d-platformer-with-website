db7f2f5760ce54ece62ab63bb58cd3894289e324
lnguyen154 <33384420+lnguyen154@users.noreply.github.com>
2022-07-31 20:24:03 -0400

    Update create_account.js

--- create_account.js
+++ create_account.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'nodelogin'
+    database: 'glitchdb'
 });
 
 const app = express();
