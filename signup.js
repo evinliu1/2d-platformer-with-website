b0f3427ef3aef747113bef24805c5321d2931d72
lnguyen154 <33384420+lnguyen154@users.noreply.github.com>
2022-07-31 20:22:09 -0400

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,48 @@
+const mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'glitchdb'
+});
+
+const app = express();
+
+connection.connect();
+global.db = connection;
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUnitialized: true,
+}));
+app.get('/sign', function(request, response) {
+    // Render account template
+    response.sendFile(path.join(__dirname + '/create_account'));
+});
+    // Record inputs into sql table
+app.post('/sign', urlencodedParser, function(request, response){
+    let username = request.body.username;
+    let email = request.body.email;
+    let password = request.body.password;
+
+    connection.query('INSERT INTO mydb', VALUES (username, email, password));
+
+    response.send("New account created!");
+
+});
+
+app.listen(3000);
