4a3bd9ef6734eedce7e8798401672895ecc07044
nathansteves <65842149+nathansteves@users.noreply.github.com>
2022-07-31 20:26:58 -0400

    Update create_account.js

--- create_account.js
+++ create_account.js
@@ -13,7 +13,22 @@ const connection = mysql.createConnection({
 
 const app = express();
 
-connection.connect();
+connection.connect(function(err) {
+    if(err){
+        console.log("Error in the connection")
+        console.log(err)
+    }
+    else{
+        console.log(`Database Connected`)
+        connection.query(`SHOW DATABASES`,
+            function (err, result) {
+                if(err)
+                    console.log(`Error executing the query - ${err}`)
+                else
+                    console.log("Result: ",result)
+            })
+    }
+})
 global.db = connection;
 
 app.set('port', process.env.PORT || 3000);
